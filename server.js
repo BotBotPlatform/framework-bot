42add36fee97ecf65704ecd124cee8ca4818a070
Shane DeWael <dewaels@purdue.edu>
2017-10-27 16:01:24 -0400

    Adds ability for users to send feedback to bot and fetch feedback categories

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ module.exports.basePath = __filename;
   */
 require('dotenv').config();
 const express = require('express');
-const request = require('request');
+const request = require('request-promise');
 const path = require('path');
 const bodyParser = require('body-parser');
 //const handleQuery = require('./database.js').handleQuery;
 const morgan = require('morgan');
+
+const feedbackTrigger = 'BBFeedback-';
+// To keep track of <UUID>:<CATEGORY ID> for active users
+var fbMap = {};
 
 /**
   * Create server to run bot application (bodyParser helps
@@ -25,11 +29,14 @@ app.post('/',(req,res) => {
   let body = req.body;
 
   if (body.object === 'page') {
-
     body.entry.forEach(function(entry) {
       if (entry.messaging[0]) {
-        // Send to message handler for bot
-        messageHandler(entry.messaging[0]);
+        if (entry.messaging[0].postback) {
+          postbackHandler(entry.messaging[0])
+        } else {
+          // Send to message handler for bot
+          messageHandler(entry.messaging[0]);
+        }
       } else {
         console.log("Webhook received unknown event: ", entry);
       }
@@ -40,39 +47,76 @@ app.post('/',(req,res) => {
 
 /**
   * Initiate feedback prompt
-  * TODO: Payloads
-  * TODO: Pull Titles from Feedback Categories w/ API
   */
-function feedbackPrompt(sid) {
-  var messageData = {
-    recipient: {
-      id: sid
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "button",
-          text: "What is your feedback related to?",
-          buttons: [{
-            type: "postback",
-            title: "Customer Service",
-            payload: "feedback-customer-service"
-          },{
-            type: "postback",
-            title: "Store Experience",
-            payload: "feedback-store-experience"
-          },{
-            type: "postback",
-            title: "Shipping",
-            payload: "feedback-shipping"
-          }]
-        }
-      }
-    }
-  };
+function feedbackPrompt(sid, token) {
+  // Feedback needs user token
+  if (!token) return;
 
-  callSendAPI(messageData);
+  // Butttons array to hold feedback categories
+  var buttons = [];
+
+  callBotAPI('feedback', {
+    method: 'GET'
+  }, token)
+  .then((res) => JSON.parse(res))
+    .then((json) => {
+      // Feedback categories, limited to first 3
+      for (var f in json.feedback.slice(0,3)) {
+        buttons = buttons.concat({
+          type: 'postback',
+          title: json.feedback[f].name,
+          payload: feedbackTrigger + json.feedback[f].id.toString()
+        });
+      }
+
+      // Message structure for FB
+      var messageData = {
+        recipient: {
+          id: sid
+        },
+        message: {
+          attachment: {
+            type: "template",
+            payload: {
+              template_type: "button",
+              text: "What is your feedback related to?",
+              buttons: buttons
+            }
+          }
+        }
+      };
+
+      // Send message to user
+      callSendAPI(messageData);
+  });
+}
+
+/**
+  * Sets <UUID>:<CATEGORY ID> in feedback map
+  */
+function setFeedbackCategory(user, category) {
+  fbMap[user] = category;
+}
+
+function sendFeedback(sid, category, feedback) {
+  callBotAPI('feedback', {
+    method: 'POST',
+    form: {category_id: category, message: feedback}
+  }, null)
+  .then((res) => JSON.parse(res))
+    .then((json) => {
+      if (json.message === 'success') {
+        var messageData = {
+          recipient: {
+            id: sid
+          },
+          message: {
+            text: 'Thanks for the feedback!'
+          }
+        };
+        callSendAPI(messageData);
+      }
+    });
 }
 
 /**
@@ -82,12 +126,37 @@ function feedbackPrompt(sid) {
   */
 function messageHandler(msg) {
   if (!msg.delivery) {
+    if (fbMap[msg.sender.id]) {
+      sendFeedback(msg.sender.id, fbMap[msg.sender.id], msg.message.text.toString());
+      delete fbMap[msg.sender.id];
+    }
     if (msg.message.text == 'feedback') {
-      feedbackPrompt(msg.sender.id);
+      feedbackPrompt(msg.sender.id, process.env.USER_TOKEN);
     }
   } else {
-
+    
   }
+}
+
+/**
+  * Handles postbacks for user events, like button pressed
+  */
+function postbackHandler(postback) {
+  let type = postback.postback.payload.toString();
+
+  if (type.startsWith(feedbackTrigger)) {
+    setFeedbackCategory(postback.sender.id, type.replace(feedbackTrigger, ''));
+    var messageData = {
+      recipient: {
+        id: postback.sender.id
+      },
+      message: {
+        text: 'What\'s your feedback?'
+      }
+    };
+    callSendAPI(messageData);
+  }
+
 }
 
 /**
@@ -105,7 +174,7 @@ function callSendAPI(messageData) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
 
-      console.log("Successfully sent generic message with id %s to recipient %s", 
+      console.log("Successfully sent message id %s to recipient %s", 
         messageId, recipientId);
     } else {
       console.error("Unable to send message.");
@@ -113,6 +182,22 @@ function callSendAPI(messageData) {
       console.error(error);
     }
   });  
+}
+
+/**
+  * Communicate with BotBot API
+  */
+function callBotAPI(endpoint, options = {}, token) {
+  const base_url = 'https://botbot.jakebrabec.me/api';
+  options.uri = base_url + '/' + endpoint
+
+  if (token) {
+    options.headers = {
+      'Authorization': 'Bearer ' + token
+    };
+  }
+
+  return request(options);
 }
 
 /**
