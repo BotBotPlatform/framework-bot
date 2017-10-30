1ce3ce6a98a71d1026f1b0dcb00774d1871928ad
Shane DeWael <dewaels@purdue.edu>
2017-10-30 16:43:23 -0400

    Add page access token

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ function callSendAPI(messageData) {
 
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    qs: { access_token: pageAccessToken },
     method: 'POST',
     json: messageData
 
